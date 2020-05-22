54fdc1e24ad9dc505a7d8d6b3a21b0e8941f9bdc
Adrian Markuszewski <adrian991999@wp.pl>
2020-05-22 17:46:51 +0200

    Add first layout and file with setting style

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App';
 const Root = () => (
-
       <App />
-
 );
 
 ReactDOM.render(<Root />, document.getElementById("root"));
