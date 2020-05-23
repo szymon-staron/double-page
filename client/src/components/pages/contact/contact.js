73d3bab11274377ad265ba0f3e8281decc58cc6a
Adrian Markuszewski <adrian991999@wp.pl>
2020-05-23 18:56:48 +0200

    work with page contact

---
+++ client/src/components/pages/contact/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './contact.module.scss';
+import Mainlayout from '../../layout/mainLayout/mainLayout';
+
+const Contact =()=>(
+    <div className={styles.container}>
+        <Mainlayout>
+
+        </Mainlayout>
+    </div>
+);
+
+
+export default Contact;
