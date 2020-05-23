1fcec6f8accc5d1bf21d25491cddeac2831eecec
Adrian Markuszewski <adrian991999@wp.pl>
2020-05-23 20:58:39 +0200

    change style and fonts

---
+++ client/src/components/layout/container/container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './container.module.scss';
+
+
+const Container =()=>(
+    <div className={styles.container}>
+        
+    </div>
+)
+
+export default Container;
---
+++ client/src/components/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './home.module.scss'
+
+
+const Home =()=>(
+    <div>
+
+    </div>
+)
+
+
+
+
+export default Home;
