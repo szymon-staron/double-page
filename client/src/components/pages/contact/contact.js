a327560dc3c807918d7f3a7cf8214ccaed259ead
Szymon Staron <szymon97staron@gmail.com>
2020-06-01 09:39:47 +0200

    add img to contact

--- client/src/components/pages/contact/contact.js
+++ client/src/components/pages/contact/contact.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./contact.module.scss";
+import img01 from "./baner_lewa01.jpg";
+import png02 from "./baner_lewa02.png";
 
 const Contact = () => (
   <div className={styles.conteiner}>
@@ -7,50 +9,51 @@ const Contact = () => (
       <header>
         <h1>Kotakt</h1>
       </header>
+      <img src={img01} alt={"..."}/>
+      <img src={png02} alt={"...."}/>
       <section>
         <article>
           <h3>Warszawa</h3>
           <p>
-           
-            Adres: ul.Lorem
+          <span className={styles.new_line}> Adres: ul.Lorem </span>
             <span className={styles.new_line}> Telefon: 997112122</span>
             <span className={styles.new_line}> Czynne: 24:00-24:00</span>
           </p>
 
           <h3>Częstochowa</h3>
           <p>
+          <span className={styles.new_line}> Adres: ul.Lorem </span>
             <span className={styles.new_line}>Telefon: 214124124</span>
             <span className={styles.new_line}>Telefon: 214124124</span>
           </p>
 
           <h3>Kraków</h3>
           <p>
-            Adres: ul.Lorem
+          <span className={styles.new_line}> Adres: ul.Lorem</span>
             <span className={styles.new_line}>Telefon: 997112122</span>
             <span className={styles.new_line}>Czynne 24:00-24:00</span>
           </p>
 
           <h3>Poznań</h3>
           <p>
-            Adres: ul.Lorem <span className={styles.new_line}>Telefon:
-            997112122</span> <span className={styles.new_line}>Czynne
-            24:00-24:00</span>
+          <span className={styles.new_line}> Adres: ul.Lorem</span>
+            <span className={styles.new_line}>Telefon: 997112122</span>
+            <span className={styles.new_line}>Czynne 24:00-24:00</span>
           </p>
         </article>
         <article>
           <h1>Serwis</h1>
           <h3>Kraków</h3>
           <p>
-            Adres: ul.Jana Pablito II <span className={styles.new_line}>
-            Telefon: 9329239432</span> <span className={styles.new_line}>Czynne:
-            17:00-20:00</span>
+          <span className={styles.new_line}>Adres: ul.Jana Pablito II</span>
+            <span className={styles.new_line}>Telefon: 9329239432</span>
+            <span className={styles.new_line}>Czynne: 17:00-20:00</span>
           </p>
           <h3>Warszawa</h3>
           <p>
-            
-            Adres: ul.Lorem <span className={styles.new_line}>Telefon:
-            997112122</span> <span className={styles.new_line}>Czynne:
-            24:00-24:00</span>
+          <span className={styles.new_line}> Adres: ul.Lorem</span>
+            <span className={styles.new_line}>Telefon: 997112122</span>
+            <span className={styles.new_line}>Czynne: 24:00-24:00</span>
           </p>
         </article>
       </section>
