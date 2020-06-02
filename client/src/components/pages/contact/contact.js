4e0f09d82c07b41589ec2e53740e03730f247b82
Szymon Staron <szymon97staron@gmail.com>
2020-06-02 09:21:35 +0200

    styling contact page

--- client/src/components/pages/contact/contact.js
+++ client/src/components/pages/contact/contact.js
@@ -5,14 +5,17 @@ import png02 from "./baner_lewa02.png";
 
 const Contact = () => (
   <div className={styles.conteiner}>
+     <div className={styles.img}>
+      <img className={styles.img__size} src={img01} alt={"..."}/>
+      <img className={styles.img__size} src={png02} alt={"...."}/>
+      </div>
     <div className={styles.Contact}>
       <header>
-        <h1>Kotakt</h1>
+        <h1 className={styles.title}>Kotakt</h1>
       </header>
-      <img src={img01} alt={"..."}/>
-      <img src={png02} alt={"...."}/>
-      <section>
-        <article>
+     
+      <section className={styles.article}>
+        <article >
           <h3>Warszawa</h3>
           <p>
           <span className={styles.new_line}> Adres: ul.Lorem </span>
@@ -57,30 +60,30 @@ const Contact = () => (
           </p>
         </article>
       </section>
-      <div>
+      <div className={styles.form}>
         <form action="" method="post">
-          <legend>Formularz kontaktowy</legend>
-          <p>
-            <label for="email">Email:</label>
-            <input
+          <legend className={styles.form__title}>Formularz kontaktowy</legend>
+          <p className={styles.form__box}>
+            <label className={styles.form__label} for="email">Email:</label>
+            <input className={styles.form__input}
               type="email"
               name="email"
               id="email"
               placeholder="Wpisz adres e-mail"
             ></input>
           </p>
-          <p>
-            <label for="topic">Temat:</label>
-            <input
+          <p className={styles.form__box}>
+            <label className={styles.form__label} for="topic">Temat:</label>
+            <input className={styles.form__input}
               type="text"
               name="topic"
               id="topic"
               placeholder="Wpisz temat"
             ></input>
           </p>
-          <p>
-            <label for="phone">Numer telefonu:</label>
-            <input
+          <p className={styles.form__box}>
+            <label className={styles.form__label} for="phone">Numer telefonu:</label>
+            <input className={styles.form__input}
               type="phone"
               name="phone"
               id="phone"
@@ -88,17 +91,17 @@ const Contact = () => (
             ></input>
           </p>
 
-          <p>
-            <label for="textArea">Treść wiadomości:</label>
-            <textarea id="textArea" name="textArea" rows="5" cols="20">
+          <p className={styles.form__box}>
+            <label className={styles.form__label} for="textArea">Treść wiadomości:</label>
+            <textarea className={styles.form__input} id="textArea" name="textArea" rows="5" cols="20">
               Wypisz treść...
             </textarea>
           </p>
-          <p>
-            <label for="checkbox">Wyślij kopie wiadomości do mnie </label>
+          <p className={styles.form__box}>
+            <label className={styles.form__label} for="checkbox">Wyślij kopie wiadomości do mnie </label>
             <input type="checkbox" id="checkbox" name="checkbox"></input>
           </p>
-          <button type="submit">Wyślij</button>
+          <button className={styles.form__button} type="submit">Wyślij</button>
         </form>
       </div>
     </div>
